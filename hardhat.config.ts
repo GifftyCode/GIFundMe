683ce0cf6028317df30866c9601e3efd089936b3
Gifftybabe <ulokangozi@gmail.com>
2024-10-10 05:26:21 +0100

    config settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,32 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
-};
+  networks: {
+    scrollSepolia: {
+      url: 'https://sepolia-rpc.scroll.io',
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      scrollSepolia: process.env.API_KEY as string,
+    },
+    customChains: [
+      {
+        network: 'scrollSepolia',
+        chainId: 534351,
+        urls: {
+          apiURL: 'https://api-sepolia.scrollscan.com/api',
+          browserURL: 'https://sepolia.scrollscan.com/',
+        },
+      },
+    ],
+  },
+}
 
 export default config;
